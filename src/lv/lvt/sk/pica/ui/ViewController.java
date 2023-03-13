0fcafb145bd84f5fea5c4034c6137538d99980cf
WuzzyLV <reinisemilsa@gmail.com>
2023-03-13 20:38:34 +0200

    adding pizzas to the list of stuff

--- src/lv/lvt/sk/pica/ui/ViewController.java
+++ src/lv/lvt/sk/pica/ui/ViewController.java
@@ -1,15 +1,21 @@
 package lv.lvt.sk.pica.ui;
 
+import lv.lvt.sk.pica.food.pizzas.Pizza;
 import lv.lvt.sk.pica.ui.mainview.MainView;
+import lv.lvt.sk.pica.ui.mainview.MenuItems;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class ViewController extends JFrame {
 
-    MainView mainView = new MainView();
+    MenuItems menuItems=new MenuItems();;
+
+    MainView mainView = new MainView(this);
 
     public ViewController() {
         super("Mamma mia pizzeria");
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(650, 750);
 
@@ -19,6 +25,10 @@ public class ViewController extends JFrame {
     public void showMainView() {
         setContentPane(mainView);
         setVisible(true);
+    }
+
+    public ArrayList<Pizza> getPizzas() {
+        return menuItems.pizzas;
     }
 }
 
--- src/lv/lvt/sk/pica/ui/mainview/MainView.java
+++ src/lv/lvt/sk/pica/ui/mainview/MainView.java
@@ -1,20 +1,44 @@
 package lv.lvt.sk.pica.ui.mainview;
 
+import lv.lvt.sk.pica.food.pizzas.Pizza;
+import lv.lvt.sk.pica.ui.ViewController;
 import lv.lvt.sk.pica.utils.WrapLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class MainView extends JPanel {
-    public MainView() {
+    ViewController viewController;
+
+    JTabbedPane menuPanel;
+    JPanel pizzaPanel;
+    JScrollPane pizzaPanelScroll;
+
+    public MainView(ViewController controller) {
+        this.viewController = controller;
+
         setLayout(new WrapLayout());
 
-        JTabbedPane leftPanel= new JTabbedPane();
-        leftPanel.setPreferredSize(new Dimension(400, 700));
-        leftPanel.setBackground(Color.RED);
+        fillMenuPanel();
+        add(menuPanel);
 
-        JPanel pizzas = new JPanel();
-        pizzas.add(new JLabel("Pizzas"));
+        //add total price panel add added items panel
+        JPanel rightPanel= new JPanel();
+        rightPanel.setPreferredSize(new Dimension(200, 700));
+        rightPanel.setBackground(Color.GREEN);
+        add(rightPanel);
+
+    }
+
+    private void fillMenuPanel() {
+        menuPanel= new JTabbedPane();
+        menuPanel.setPreferredSize(new Dimension(400, 700));
+        menuPanel.setBackground(Color.RED);
+
+        pizzaPanel= new JPanel();
+        pizzaPanelScroll = new JScrollPane(pizzaPanel);
+        fillPizzaPanel();
 
         JPanel sides = new JPanel();
         sides.add(new JLabel("Sides"));
@@ -22,24 +46,39 @@ public class MainView extends JPanel {
         JPanel extras = new JPanel();
         extras.add(new JLabel("Extras"));
 
-        leftPanel.addTab("Pizzas", pizzas);
-        leftPanel.addTab("Sides", sides);
-        leftPanel.addTab("Extras", extras);
+        menuPanel.addTab("Pizzas", pizzaPanelScroll);
+        menuPanel.addTab("Sides", sides);
+        menuPanel.addTab("Extras", extras);
+    }
+
+    private void fillPizzaPanel() {
+        List<Pizza> pizzas = viewController.getPizzas();
+        if (pizzas==null){
+            return;
+        }
 
 
-        JPanel rightPanel= new JPanel();
-        rightPanel.setPreferredSize(new Dimension(200, 700));
-        rightPanel.setBackground(Color.GREEN);
+        for (int i = 0; i < pizzas.size(); i++) {
+            Pizza pizza = pizzas.get(i);
+            JPanel currPizzaPanel = new JPanel();
+            currPizzaPanel.setLayout(new BoxLayout(currPizzaPanel, BoxLayout.X_AXIS));
+            currPizzaPanel.setPreferredSize(new Dimension(400, 100));
 
-        add(leftPanel);
-        add(rightPanel);
+            JLabel pizzaImg = new JLabel(pizza.getImage());
+            JLabel pizzaName = new JLabel(pizza.getName());
+            JLabel price = new JLabel(pizza.getPrice().toString());
 
+            currPizzaPanel.add(pizzaImg);
+            currPizzaPanel.add(pizzaName);
+            currPizzaPanel.add(price);
 
+            currPizzaPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-
-
+            pizzaPanel.add(currPizzaPanel);
+        }
 
     }
+
 
 
 
