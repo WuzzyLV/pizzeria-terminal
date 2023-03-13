de490cba88cb13fecc77921047b69b6f6bf606f5
WuzzyLV <reinisemilsa@gmail.com>
2023-03-13 20:36:40 +0200

    add image to the pizza class

--- src/lv/lvt/sk/pica/food/pizzas/Pizza.java
+++ src/lv/lvt/sk/pica/food/pizzas/Pizza.java
@@ -2,18 +2,25 @@ package lv.lvt.sk.pica.food.pizzas;
 
 import lv.lvt.sk.pica.food.Item;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.Stack;
 
 public class Pizza {
     String name;
     Double price=0.0;
+    ImageIcon image;
     Stack<Item> items = new Stack<>();
 
     public Pizza(String name) {
         this.name = name;
+        image = new ImageIcon("./resources/404.png");
+        System.out.println();
     }
-
+    public Pizza(String name, ImageIcon image) {
+        this.name = name;
+        this.image = image;
+    }
     public void addTopping(Item item) {
         items.push(item);
         price += item.getPrice();
@@ -26,5 +33,38 @@ public class Pizza {
     }
     public Stack<Item> getToppings() {
         return items;
+    }
+
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Stack<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Stack<Item> items) {
+        this.items = items;
     }
 }
