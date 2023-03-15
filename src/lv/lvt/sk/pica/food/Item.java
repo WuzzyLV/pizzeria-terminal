5711381dc40bad75492773acbe22323901f30a18
[Reinis] <[r.allis@sk.lvt.lv]>
2023-03-15 09:20:40 +0200

    refactoring Pizza to inherit Item

--- src/lv/lvt/sk/pica/food/Item.java
+++ src/lv/lvt/sk/pica/food/Item.java
@@ -1,19 +1,20 @@
 package lv.lvt.sk.pica.food;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Item {
     private String name;
     private double price;
     private String desc;
-    private Image image;
+    private ImageIcon image;
 
     public Item(String name, double price, String desc) {
         this.name = name;
         this.price = price;
         this.desc = desc;
     }
-    public Item(String name, double price, String desc, Image image) {
+    public Item(String name, double price, String desc, ImageIcon image) {
         this.name = name;
         this.price = price;
         this.desc = desc;
@@ -24,15 +25,35 @@ public class Item {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public String getDesc() {
         return desc;
     }
 
-    public Image getImage() {
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public ImageIcon getImage() {
         return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    public void addToPrice(double price) {
+        this.price += price;
     }
 }
