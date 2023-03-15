e8666e06c83893130f8922194bd8add4e13c3972
[Reinis] <[r.allis@sk.lvt.lv]>
2023-03-15 09:20:51 +0200

    adding more to menuView

--- src/lv/lvt/sk/pica/ui/mainview/MainView.java
+++ src/lv/lvt/sk/pica/ui/mainview/MainView.java
@@ -6,6 +6,8 @@ import lv.lvt.sk.pica.utils.WrapLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 public class MainView extends JPanel {
@@ -13,6 +15,8 @@ public class MainView extends JPanel {
 
     JTabbedPane menuPanel;
     JPanel pizzaPanel;
+    JPanel sides;
+    JPanel extras;
     JScrollPane pizzaPanelScroll;
 
     public MainView(ViewController controller) {
@@ -28,7 +32,6 @@ public class MainView extends JPanel {
         rightPanel.setPreferredSize(new Dimension(200, 700));
         rightPanel.setBackground(Color.GREEN);
         add(rightPanel);
-
     }
 
     private void fillMenuPanel() {
@@ -40,10 +43,10 @@ public class MainView extends JPanel {
         pizzaPanelScroll = new JScrollPane(pizzaPanel);
         fillPizzaPanel();
 
-        JPanel sides = new JPanel();
+        sides = new JPanel();
         sides.add(new JLabel("Sides"));
 
-        JPanel extras = new JPanel();
+        extras = new JPanel();
         extras.add(new JLabel("Extras"));
 
         menuPanel.addTab("Pizzas", pizzaPanelScroll);
@@ -53,46 +56,73 @@ public class MainView extends JPanel {
 
     private void fillPizzaPanel() {
         List<Pizza> pizzas = viewController.getPizzas();
-        if (pizzas==null){
-            return;
-        }
 
-
-        for (int i = 0; i < pizzas.size(); i++) {
-            Pizza pizza = pizzas.get(i);
+        for (Pizza pizza : pizzas) {
             JPanel currPizzaPanel = new JPanel();
-            currPizzaPanel.setLayout(new BoxLayout(currPizzaPanel, BoxLayout.X_AXIS));
-            JPanel helperPanel = new JPanel();
-            //helperPanel.setLayout(new BoxLayout(helperPanel, BoxLayout.X_AXIS));
-            helperPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            currPizzaPanel.setLayout(new BoxLayout(currPizzaPanel, BoxLayout.Y_AXIS));
+            currPizzaPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+            JPanel helperPanel = new JPanel();
+            helperPanel.setLayout(new BoxLayout(helperPanel, BoxLayout.X_AXIS));
+            helperPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
             currPizzaPanel.add(helperPanel);
 
             JLabel pizzaImg = new JLabel(pizza.getImage());
             pizzaImg.setAlignmentY(Component.CENTER_ALIGNMENT);
 
+            JPanel infoPanel = new JPanel();
+            infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
             JLabel pizzaName = new JLabel(pizza.getName() + " ");
-            pizzaName.setAlignmentY(Component.CENTER_ALIGNMENT);
+            JLabel tops = new JLabel(pizza.getDesc());
+            tops.setPreferredSize(new Dimension(175, 40));
 
-            JLabel price = new JLabel(pizza.getPrice().toString() + " $");
+            JLabel price = new JLabel(pizza.getPrice() + " $");
             price.setAlignmentY(Component.CENTER_ALIGNMENT);
 
             //setting fonts
-            Font font = new Font( pizzaName.getFont().getFontName(), Font.BOLD, 20);
+            Font font = new Font(pizzaName.getFont().getFontName(), Font.BOLD, 20);
             pizzaName.setFont(font);
             price.setFont(font);
 
+            infoPanel.add(pizzaName);
+            infoPanel.add(tops);
+
+            helperPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Add some spacing between the components
             helperPanel.add(pizzaImg);
             helperPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Add some spacing between the components
-            helperPanel.add(pizzaName);
+            helperPanel.add(infoPanel);
             helperPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Add some spacing between the components
             helperPanel.add(price);
-
+            helperPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Add some spacing between the components
             helperPanel.setAlignmentY(Component.CENTER_ALIGNMENT);
 
-            //TODO make the height scaled based on image or center it properly
-            currPizzaPanel.setPreferredSize(new Dimension(380, 100));
+            //currPizzaPanel.setSize(new Dimension(400, pizza.getImage().getIconHeight()));
             currPizzaPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+            buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            JButton size20 = new JButton("20 cm");
+            buttonPanel.add(size20);
+            JButton size30 = new JButton("30 cm");
+            buttonPanel.add(size30);
+            JButton size50 = new JButton("50 cm");
+            buttonPanel.add(size50);
+            currPizzaPanel.add(buttonPanel);
+            buttonPanel.setVisible(true);
+
+            currPizzaPanel.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent evt) {
+                    System.out.println("moved in");
+                    //buttonPanel.setVisible(true);
+                    buttonPanel.repaint();
+                }
+                public void mouseExited(MouseEvent evt) {
+                    System.out.println("moved out");
+                    //buttonPanel.setVisible(false);
+                    currPizzaPanel.repaint();
+                }
+            });
 
             pizzaPanel.add(currPizzaPanel);
         }
